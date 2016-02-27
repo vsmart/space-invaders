ec44b6904bdcf34c7f742540227fa6a97aa94eb1
Franziska Schmidt <franziska.petra@gmail.com>
2016-02-27 22:37:35 +0100

    Introduce Fire + Move Size Constants

--- src/space_invaders/core.clj
+++ src/space_invaders/core.clj
@@ -5,12 +5,20 @@
 (defn setup []
   (q/frame-rate 30)
   (q/color-mode :hsb)
-  {:battleship {:x (/ (q/width) 2) :y (* (q/height) 2/3)}})
+  {:battleship
+    {:x (/ (q/width) 2)
+     :y (* (q/height) 2/3)}})
 
 (defn draw-state [state]
-  (q/background 240)
-  (q/fill 60 255 255)
-  (q/rect (:x (:battleship state)) (:y (:battleship state)) 20 20)
+ ; (q/background 240)
+  (draw-battleship state)
+  (print-current-state state))
+
+(defn draw-battleship [state]
+  (q/fill 120 255 255)
+  (q/ellipse (:x (:battleship state)) (:y (:battleship state)) 35 15))
+
+(defn print-current-state [state]
   (q/fill 0 0 0)
   (q/text (str state) 10 20))
 
@@ -20,23 +28,37 @@
     newer-state))
 
 (defn move-spaceship-left [state]
-  (move-spaceship -1 0 state))
+  (move-spaceship one-neg-move 0 state))
 
 (defn move-spaceship-right [state]
-  (move-spaceship 1 0 state))
+  (move-spaceship one-move 0 state))
 
 (defn move-spaceship-up [state]
-  (move-spaceship 0 -1 state))
+  (move-spaceship 0 one-neg-move state))
 
 (defn move-spaceship-down [state]
-  (move-spaceship 0 1 state))
+  (move-spaceship 0 one-move state))
+
+(defn fire [state]
+  (loop [x (:x (:battleship state))
+         y (:y (:battleship state))]
+    (if (> 0 y)
+      state
+      (do
+        (q/fill 200 255 255)
+        (q/ellipse x y (/ y 10) (/ y 10))
+        (recur x (- y 5))))))
 
 (defn move-after-key-pressed [state event]
   (let [key (:key event)]
     (cond
       (= key :left) (move-spaceship-left state)
       (= key :right) (move-spaceship-right state)
+      (= key :up) (fire state)
       :else state)))
+
+(def one-move 20)
+(def one-neg-move (* one-move -1))
 
 (q/defsketch space-invaders
   :size [500 500]
--- test/space_invaders/core_test.clj
+++ test/space_invaders/core_test.clj
@@ -3,33 +3,46 @@
             [quil.middleware :as m]
             [space-invaders.core :as core]
             [midje.sweet :refer :all]))
-(def state
-  {:battleship {:x 0 :y 0}})
+
+(def state {:battleship {:x 0 :y 0}})
+
+(facts "about one-move and one-neg-move"
+  (fact "one-move is a positive number"
+    (> core/one-move 0)
+    => true)
+
+  (fact "one-neg-move is a negative number"
+    (< core/one-neg-move 0)
+    => true)
+
+  (fact "one-neg-move is the negation of one-move"
+    (/ core/one-move core/one-neg-move)
+    => -1))
 
 (facts "about move-spaceship"
   (fact "about move-spaceship left"
     (:battleship (core/move-spaceship-left state))
-    => {:x -1 :y 0} )
+    => {:x core/one-neg-move :y 0} )
 
   (fact "about move-spaceship right"
     (:battleship (core/move-spaceship-right state))
-    => {:x 1 :y 0} )
+    => {:x core/one-move :y 0} )
 
   (fact "about move-spaceship up"
    (:battleship (core/move-spaceship-up state))
-        => {:x 0 :y -1} )
+   => {:x 0 :y core/one-neg-move} )
 
   (fact "about move-spaceship down"
-        (:battleship (core/move-spaceship-down state))
-        => {:x 0 :y 1} )
+    (:battleship (core/move-spaceship-down state))
+    => {:x 0 :y core/one-move} )
 
   (fact "about move-spaceship rand"
-        (let [rand-x (rand 100)
-              rand-y (rand 100)]
-          (:battleship (core/move-spaceship rand-x rand-y state))
-          => {:x rand-x :y rand-y}))
+    (let [rand-x (rand 100)
+          rand-y (rand 100)]
+      (:battleship (core/move-spaceship rand-x rand-y state))
+      => {:x rand-x :y rand-y}))
 
-       (fact "about move-spaceship 1 2"
+      (fact "about move-spaceship 1 2"
              (core/move-spaceship 1 2 state)
              => {:battleship {:x 1 :y 2}})
 
@@ -45,14 +58,9 @@
              (core/move-spaceship -150 -200 state)
              => {:battleship {:x -150 :y -200}} ))
 
-(def left-key-pressed
-  {:key :left})
-
-(def right-key-pressed
-  {:key :right})
-
-(def other-key-pressed
-  {:key :c})
+(def left-key-pressed  {:key :left})
+(def right-key-pressed {:key :right})
+(def other-key-pressed {:key :c})
 
 (facts "about key-pressed"
   (fact "about other key pressed"
@@ -61,8 +69,8 @@
 
   (fact "about left key pressed"
     (core/move-after-key-pressed state left-key-pressed)
-    => {:battleship {:x -1 :y 0}})
+    => {:battleship {:x core/one-neg-move :y 0}})
 
   (fact "about right key pressed"
     (core/move-after-key-pressed state right-key-pressed)
-    => {:battleship {:x 1 :y 0}}))
+    => {:battleship {:x core/one-move :y 0}}))
